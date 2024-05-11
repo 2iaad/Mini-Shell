2c892c668c08f1b0fc1b19a7dabf82b3c9e62ed5
Ziyad Derfoufi <zderfouf@e1r4p15.1337.ma>
2024-05-11 20:09:21 +0100

    Sorting the linked list alphabeticaly for the export command

--- main.c
+++ main.c
@@ -1,49 +1,23 @@
 #include "includes/minishell.h"
 #include "includes/tools.h"
 
-int	ft_lstsize(t_env *lst)
+void	alpha_arrang(t_env *env)
 {
-	int		nofn;
-	t_env	*ptr2n;
+	char *temp;
+	t_env *tmp;
 
-	nofn = 0;
-	ptr2n = lst;
-	if (ptr2n == NULL)
-		return (0);
-	while (ptr2n != NULL)
+	tmp = env;
+	while (tmp && tmp->next)
 	{
-		ptr2n = ptr2n->next; 
-		nofn++;
-	}
-	return (nofn);
-}
-
-void	d_node(t_env	**env_args)
-{
-	t_env *tmp0; // hadi fiha salam li 9bl mn HELLO
-	t_env *tmp1;
-	t_env *tmp2; // hadi fiha salam li mnwra HELLO
-
-	tmp0 = *env_args;
-	tmp1 = *env_args;
-	while (tmp0)
-	{
-		if (!ft_strcmp(tmp0->key, "HELLO", 4))
+		if (tmp->key[0] > tmp->next->key[0])
 		{
-			tmp2 = tmp0->next;
-			break;
+			temp = tmp->key;
+			tmp->key = tmp->next->key;
+			tmp->next->key = temp;
+			tmp = env;
 		}
-		tmp0 = tmp0->next;
-	}
-	while (tmp1)
-	{
-		if (!ft_strcmp(tmp1->next->key, "HELLO", 4))
-		{
-			free(tmp1->next);
-			tmp1->next = tmp2;
-			break;
-		}
-		tmp1 = tmp1->next;
+		else
+			tmp = tmp->next;
 	}
 }
 
@@ -54,33 +28,34 @@ int main(int ac, char **av)
 	int i = 0;
 	t_env *head;
 	t_env *tmp;
+	t_env *tmp2;
 
-	atexit(f);
+	// atexit(f);
 	head = NULL;
-	while (i < 7)
+	while (i < 3)
 	{
-		if (i == 4)
-			ft_lstadd_back(&head, ft_lstnew("HELLO","cv"));
-		else
-			ft_lstadd_back(&head, ft_lstnew("salam","cv"));
+		ft_lstadd_back(&head, ft_lstnew(ft_strdup("armani"),ft_strdup("cv")));
+		ft_lstadd_back(&head, ft_lstnew(ft_strdup("maserati"),ft_strdup("cv")));
+		ft_lstadd_back(&head, ft_lstnew(ft_strdup("black_jack"),ft_strdup("cv")));
 		i++;
 	}
-	// printf("%s",head->key);
+
+
+	tmp2 = head;
+	while (tmp2)
+	{
+		printf("%s--->%s\n", tmp2->key, tmp2->value);
+		tmp2 = tmp2->next;
+	}
+
+	printf("\n\n\nafter:\n\n\n");
+
+	alpha_arrang(head);
 	tmp = head;
 	while (tmp)
 	{
 		printf("%s--->%s\n", tmp->key, tmp->value);
 		tmp = tmp->next;
 	}
-	printf("-->%d\n", ft_lstsize(head));
-
-	d_node(&head);
-	// printf("\n\n\n\n");
-	// tmp = *head;
-	// while (tmp)
-	// {
-	// 	printf("%s--->%s\n", tmp->key, tmp->value);
-	// 	tmp = tmp->next;
-	// }
-	// printf("-->%d\n", ft_lstsize(*head));
+	ft_lstclear(&head);
 }
