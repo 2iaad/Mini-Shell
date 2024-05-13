d91186bb4dc1bf56d9fe28a7b150278ca478834a
Ziyad Derfoufi <zderfouf@e1r13p3.1337.ma>
2024-05-13 12:38:54 +0100

    echo

--- main.c
+++ main.c
@@ -1,61 +1,31 @@
 #include "includes/minishell.h"
 #include "includes/tools.h"
+#include <unistd.h>
 
-void	alpha_arrang(t_env *env)
+// void    echo(t_list	*lst) // handle "echo -n -n"
+// {
+//     int flag;
+
+//     flag = flag_check(lst->cmd[1]);
+//     if (lst->cmd[1] == NULL) // echo
+//         return (void) write(1, "\n", 1);
+// 	if (!flag) // echo -n and echo -nnnnnn
+// 	{
+// 		if (lst->cmd[2])
+// 			ft_putstr(lst->cmd[2], 1);
+// 	}
+// 	else
+// 	{
+// 		if (lst->cmd[1][0] == '$' && (lst->cmd[1][1] != '$' && lst->cmd[1][1] != '?')) // ila kan lst->cmd[1] o lst->cmd[1][0] = '$' => expand
+// 			expand(lst, lst->cmd[1] + 1);
+// 		else
+// 			ft_putstr(lst->cmd[1], 1); // else echo string
+// 	}
+// 	if (flag)
+// 		write(1, "\n", 1);
+// }
+
+int main()
 {
-	char *temp;
-	t_env *tmp;
-
-	tmp = env;
-	while (tmp && tmp->next)
-	{
-		if (tmp->key[0] > tmp->next->key[0])
-		{
-			temp = tmp->key;
-			tmp->key = tmp->next->key;
-			tmp->next->key = temp;
-			tmp = env;
-		}
-		else
-			tmp = tmp->next;
-	}
-}
-
-void f(){system("leaks a.out");}
-
-int main(int ac, char **av)
-{
-	int i = 0;
-	t_env *head;
-	t_env *tmp;
-	t_env *tmp2;
-
-	// atexit(f);
-	head = NULL;
-	while (i < 3)
-	{
-		ft_lstadd_back(&head, ft_lstnew(ft_strdup("armani"),ft_strdup("cv")));
-		ft_lstadd_back(&head, ft_lstnew(ft_strdup("maserati"),ft_strdup("cv")));
-		ft_lstadd_back(&head, ft_lstnew(ft_strdup("black_jack"),ft_strdup("cv")));
-		i++;
-	}
-
-
-	tmp2 = head;
-	while (tmp2)
-	{
-		printf("%s--->%s\n", tmp2->key, tmp2->value);
-		tmp2 = tmp2->next;
-	}
-
-	printf("\n\n\nafter:\n\n\n");
-
-	alpha_arrang(head);
-	tmp = head;
-	while (tmp)
-	{
-		printf("%s--->%s\n", tmp->key, tmp->value);
-		tmp = tmp->next;
-	}
-	ft_lstclear(&head);
+	printf("%d",getpid());
 }
