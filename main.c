1a19bb97de7eb5bc5eb470f1637e8b1746d97e7e
Ziad Derfoufi <ziadderfoufi@Ziads-MacBook-Pro.local>
2024-05-20 16:31:13 +0100

    double pointer to function test

--- main.c
+++ main.c
@@ -2,30 +2,22 @@
 #include "includes/tools.h"
 #include <unistd.h>
 
-// void    echo(t_list	*lst) // handle "echo -n -n"
-// {
-//     int flag;
 
-//     flag = flag_check(lst->cmd[1]);
-//     if (lst->cmd[1] == NULL) // echo
-//         return (void) write(1, "\n", 1);
-// 	if (!flag) // echo -n and echo -nnnnnn
-// 	{
-// 		if (lst->cmd[2])
-// 			ft_putstr(lst->cmd[2], 1);
-// 	}
-// 	else
-// 	{
-// 		if (lst->cmd[1][0] == '$' && (lst->cmd[1][1] != '$' && lst->cmd[1][1] != '?')) // ila kan lst->cmd[1] o lst->cmd[1][0] = '$' => expand
-// 			expand(lst, lst->cmd[1] + 1);
-// 		else
-// 			ft_putstr(lst->cmd[1], 1); // else echo string
-// 	}
-// 	if (flag)
-// 		write(1, "\n", 1);
-// }
+void	f(char **str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		printf("%s\n", str[i]);
+		i++;
+	}
+}
+
 
 int main()
 {
-	printf("%d",getpid());
+	int i = 0;
+	char **str = ft_split("salam ana smiti ziad", ' ');
+
+	f(str + 1);	
 }
