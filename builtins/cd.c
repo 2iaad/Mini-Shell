7c1c0b6d5f118a7fa451c63b6df4a5ce6cc2d3a2
Ziyad Derfoufi <zderfouf@e1r13p1.1337.ma>
2024-05-29 11:41:15 +0100

    [S FEAT] error message added that indicates an error

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 10:10:29 by zderfouf          #+#    #+#             */
-/*   Updated: 2024/05/15 10:10:47 by zderfouf         ###   ########.fr       */
+/*   Updated: 2024/05/29 11:37:28 by zderfouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,16 @@
 void    cd(char **cmd)
 {
     char *c_path;
+
     if (!access(cmd[1] , F_OK)) // === true
     {
         if (chdir(cmd[1]) == -1) // === then go to 
             perror("chdir");
     }
     else
-        perror("cd");
+	{
+        write (2, "cd: ", 4);
+        ft_putstr_fd (cmd[1] , 2);
+        write (2, ": No such file or directory\n", 28);
+	}
 }
