e8637212285233716d9ca68c5be5b588b34ae9d2
Ziyad Derfoufi <zderfouf@e1r7p2.1337.ma>
2024-06-06 21:24:02 +0100

    [refactory] [restyle] rewriting the execution function in a shorter way piping is working perfectly fine

--- execution/pipe_execute/identifier.c
+++ execution/pipe_execute/identifier.c
@@ -6,7 +6,7 @@
 /*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 10:13:33 by zderfouf          #+#    #+#             */
-/*   Updated: 2024/06/06 16:38:28 by zderfouf         ###   ########.fr       */
+/*   Updated: 2024/06/06 21:18:26 by zderfouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,61 +34,58 @@ bool	builtins(t_final *lst, t_env *env_list)
 	return (flag);
 }
 
-void    execution(t_final *lst, t_env *env, char **envp)
+void	pipe_cmd(t_final *lst, int *fds, int flag)
 {
-	int	fds[2];
-	int	sec_fd[2];
-	int	pid;
-// ls > 1 > 2 > 23 > 3 > >>23 =====> exit from parsing
-// echo salam > a > b | wc --> error
-	sec_fd[0] = dup(0);
-	sec_fd[1] = dup(1);
-	while (lst)
-	{
+	if (flag == 0)
 		if (lst->next)
 			pipe(fds);
-		pid = fork();
-		if (!pid)
-		{
-			infile_opener(lst->in_file);
-			outfile_opener(lst->out_file); // the problem here hiya ana kan dupi outfile m3a l stdout o lstdout kitlef
-			// makanb9ach 9ad nkteb f pipe 7it aslan stdout talf makinch --> donc khasni nkhabih 
-			if (lst->next)
+	if (flag == 1)
+		if (lst->next)
 			{
 				dup2(fds[1], 1);
 				close(fds[0]);
 				close(fds[1]);
 			}
+	if (flag == 2)
+		if (lst->next)
+		{
+			dup2(fds[0], 0);
+			close(fds[0]);
+			close(fds[1]);	
+		}
+}
+
+void    execution(t_final *lst, t_env *env, char **envp)
+{
+	int	pid;
+	int	fds[2] = {1337, 1337};
+	int	sec_fd[2];
+
+	(1 == 1) && (sec_fd[0] = dup(0)) && (sec_fd[1] = dup(1));
+	while (lst)
+	{
+		pipe_cmd(lst, &fds[0], 0);
+		pid = fork();
+		if (!pid)
+		{
+			if (lst->in_file || lst->out_file)
+			{
+				infile_opener(lst->in_file);
+				outfile_opener(lst->out_file);
+			}
+			else
+				pipe_cmd(lst, &fds[0], 1);
 			execute_cmd(lst, envp);
 		}
 		else
 		{
-			if (lst->next)
-			{
-				dup2(fds[0], 0);
-				close(fds[0]);
-				close(fds[1]);	
-			}
+			pipe_cmd(lst, &fds[0], 2);
 			while (wait(NULL) == -1)
 				;
 			lst = lst->next;
 		}
 	}
-	dup2(sec_fd[0], 0);
-	dup2(sec_fd[1], 1);
+	if (dup2(sec_fd[0], 0) == -1 || dup2(sec_fd[1], 1) == -1)
+		perror("perror");
 }
 
-
-/*		PRINT FINAL CMD		*/
-
-	// while (lst)
-	// {
-	// 	i = 0;
-	// 	while (lst->final_cmd[i])
-	// 	{
-	// 		printf("---->%s", lst->final_cmd[i]);
-	// 		i++;
-	// 	}
-	// 	printf("\n");
-	// 	lst = lst->next;	
-	// }
