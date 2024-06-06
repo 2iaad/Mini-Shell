019724746476361f6ee97ab52cf393e5b0397e4e
Ziyad Derfoufi <zderfouf@e1r7p3.1337.ma>
2024-06-07 00:21:09 +0100

    builtins caller function removed to this file

---
+++ execution/builtins/caller.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   caller.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 00:02:14 by zderfouf          #+#    #+#             */
+/*   Updated: 2024/06/07 00:02:27 by zderfouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../minishell.h"
+
+bool	builtins(t_final *lst, t_env *env_list)
+{
+	bool	flag;
+
+	flag = false;
+    if (!ft_strncmp(lst->final_cmd[0], "echo", 4))
+        echo(lst), flag = true;
+    if (!ft_strncmp(lst->final_cmd[0], "cd", 2))
+        cd(lst, &env_list), flag = true;
+    if (!ft_strncmp(lst->final_cmd[0], "pwd", 3))
+        pwd(), flag = true;
+    if (!ft_strncmp(lst->final_cmd[0], "export", 6))
+        export_command(lst, &env_list), flag = true;
+    if (!ft_strncmp(lst->final_cmd[0], "unset", 5))
+        unset(lst, env_list), flag = true;
+    if (!ft_strncmp(lst->final_cmd[0], "env", 3))
+        env(lst, env_list), flag = true;
+    if (!ft_strncmp(lst->final_cmd[0], "exit", 4))
+        exit_command(lst->final_cmd), flag = true;
+	return (flag);
+}
