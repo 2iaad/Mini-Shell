25e232f7e9ba8e783795d3f49c03004e48028eb1
Ziyad Derfoufi <zderfouf@e3r9p8.1337.ma>
2024-06-07 01:52:27 +0100

    deleted some unnecessary prototypes from the minishell.h

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 16:45:46 by ibouram           #+#    #+#             */
-/*   Updated: 2024/06/07 00:30:34 by zderfouf         ###   ########.fr       */
+/*   Updated: 2024/06/07 01:44:28 by zderfouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -198,14 +198,10 @@ void	execute_cmd(t_final	*lst, char **env);
 
 void	error(char *str, int a);
 
-// void	ft_free(char **tab);
-// void	error(char *str, int a);
-// char	*look_for_paths(char **env);
+
+
 // void	pipex(char *cmd, char **env);
-// void	first_child(char **av, char **ev);
 // void	heredoc_limiter(char **av, int ac);
-// char	*right_path(char *cmd, char **env);
 // void	heredoc_file_opener(int ac, char **av);
-// int		normal_file_opener(int a, char *file);
 
 #endif
