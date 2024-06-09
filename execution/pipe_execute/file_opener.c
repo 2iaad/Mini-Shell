753cb5c1c68213789ab74b1f0a0c6ef2f240f4f6
Ziad Derfoufi <ziadderfoufi@Ziads-MacBook-Pro.local>
2024-06-09 14:57:50 +0100

    [fix] fixed the here_doc when opening multiple heredocs previous: the last heredoc recieves all the input of the other files now: only recieves the input i gives to the last heredoc---> same as BASH

--- execution/pipe_execute/file_opener.c
+++ execution/pipe_execute/file_opener.c
@@ -6,7 +6,7 @@
 /*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 21:56:15 by zderfouf          #+#    #+#             */
-/*   Updated: 2024/06/07 18:03:16 by zderfouf         ###   ########.fr       */
+/*   Updated: 2024/06/09 14:54:27 by zderfouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,8 @@ void	heredoc_limiter(char *DELIMITER, int fd)
 			free (line);
 			break ;
 		}
-		ft_putendl_fd(line, fd);
+		if (fd != 1337)
+			ft_putendl_fd(line, fd);
 		free(line);
 	}
 }
@@ -39,13 +40,20 @@ void	heredoc_opener(char **heredoc)
 	i = 0;
 	if (!heredoc)
 		return ;
-	while (heredoc[i])
-		i++;
+	while (heredoc[i + 1])
+	{
+		heredoc_limiter(heredoc[i], 1337); // give the delimiter here
+			i++;
+	}
 	fd = open("/tmp/here_doc", O_RDWR | O_CREAT | O_TRUNC, 0644);
-	heredoc_limiter(heredoc[i - 1], fd); // give the delimiter here
+	if (fd == 1)
+		error("open", 1337);
+	heredoc_limiter(heredoc[i], fd); // here heredoc[i] atkoun katpointi 3la akhir delimiter
+
 	close(fd);
 	fd = open("/tmp/here_doc", O_RDONLY | O_CREAT, 0644);
-	dup2(fd, 0);
+	if (dup2(fd, 0) == -1)
+		error("open", 1337);
 	close(fd);
 }
 
