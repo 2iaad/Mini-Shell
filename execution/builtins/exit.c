ce4d78044437ef998bcf36555ac58e9da2523330
Idder Bouram <ibouram@e3r11p8.1337.ma>
2024-07-31 16:26:18 +0100

    [handle] this case --> exit 44 dd

--- execution/builtins/exit.c
+++ execution/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zderfouf <zderfouf@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ibouram <ibouram@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 11:49:51 by zderfouf          #+#    #+#             */
-/*   Updated: 2024/07/29 11:40:29 by zderfouf         ###   ########.fr       */
+/*   Updated: 2024/07/31 16:25:35 by ibouram          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 void	exit_command(char **cmd)
 {
 	int	i;
-	int status;
+	int	status;
 
 	i = -1;
 	status = exit_status(1, 0);
+	if (cmd[2])
+		return (ft_putstr_fd("exit\nminishell: exit: too many arguments\n", 2));
 	while (cmd[1] && cmd[1][++i])
 	{
 		if (!ft_isnum(cmd[1][i]))
